3c44691ed1dd5d4c1dc649942cd31ea3b4c99c52
Jan <jgrodowski@gmail.com>
2016-12-15 11:50:18 +0100

    Initial Commit

---
+++ Mojify/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  Mojify
+//
+//  Created by Jan Grodowski on 15/12/2016.
+//  Copyright © 2016 Jan Grodowski. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
